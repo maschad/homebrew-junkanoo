035e186429327c5c80a76e2e4cb6d27fee2433fe
chad <chad.nehemiah94@gmail.com>
2025-05-27 20:09:01 -0500

    build: update installation

--- Formula/junkanoo.rb
+++ Formula/junkanoo.rb
@@ -2,14 +2,14 @@ class Junkanoo < Formula
   desc "Decentralized ephemeral file sharing CLI browser"
   homepage "https://github.com/maschad/junkanoo"
   url "https://github.com/maschad/junkanoo/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "c036202138efc4f50f96d7687c25d09583313061ca690b056ef66d975ca29253"
+  sha256 "" # You'll need to fill this in after creating the release
   license "MIT"
   head "https://github.com/maschad/junkanoo.git", branch: "main"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "junkanoo"
   end
 
   test do
