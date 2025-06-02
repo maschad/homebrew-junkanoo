bccbb3fd900ca47bc02887f412126c2358fee247
chad <chad.nehemiah94@gmail.com>
2025-06-02 18:44:56 -0500

    build: update homebrew tap

--- Formula/junkanoo.rb
+++ Formula/junkanoo.rb
@@ -1,9 +1,8 @@
 class Junkanoo < Formula
   desc "Decentralized ephemeral file sharing CLI browser"
   homepage "https://github.com/maschad/junkanoo"
-  url "https://github.com/maschad/junkanoo/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "" # You'll need to fill this in after creating the release
-  license "MIT"
+  url "https://github.com/maschad/junkanoo/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "1e447133eb1b7033427b689c437e9f769b922ec8d89de5797e03859b2d3f7ee7"
   head "https://github.com/maschad/junkanoo.git", branch: "main"
 
   depends_on "rust" => :build
