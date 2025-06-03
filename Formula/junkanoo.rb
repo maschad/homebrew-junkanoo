cee737f9b375c01999f1986b22b812eefa3f5f34
chad <chad.nehemiah94@gmail.com>
2025-06-03 13:59:49 -0500

    chore: update formula

--- Formula/junkanoo.rb
+++ Formula/junkanoo.rb
@@ -1,15 +1,12 @@
 class Junkanoo < Formula
   desc "Decentralized ephemeral file sharing CLI browser"
   homepage "https://github.com/maschad/junkanoo"
-  url "https://github.com/maschad/junkanoo/archive/refs/tags/v1.2.2.tar.gz"
+  url "https://github.com/maschad/junkanoo/releases/download/v1.2.2/junkanoo"
   sha256 "433da011100899c8054a40572b0a8d77d8c4ae493e8306e1efb40d89fb5a6adc"
   head "https://github.com/maschad/junkanoo.git", branch: "main"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "build", "--release"
-    bin.install "target/release/junkanoo"
+    bin.install "junkanoo"
   end
 
   test do
