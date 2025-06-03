92d5f66076fd140c595ebc3a65b6a61b103733a4
chad <chad.nehemiah94@gmail.com>
2025-06-03 14:07:31 -0500

    chore: format formula properly

--- Formula/junkanoo.rb
+++ Formula/junkanoo.rb
@@ -2,7 +2,6 @@ class Junkanoo < Formula
   desc "Decentralized ephemeral file sharing CLI browser"
   homepage "https://github.com/maschad/junkanoo"
   url "https://github.com/maschad/junkanoo/releases/download/v1.2.2/junkanoo"
-  version "1.2.2"
   sha256 "3198bb7dbb482ed87a439d030a68db01e88eeffdbffb9a829739b0bfcd1b665b"
   head "https://github.com/maschad/junkanoo.git", branch: "main"
 
