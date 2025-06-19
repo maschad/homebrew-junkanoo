ed1680ef1b98609fa4aab0b03c34f42a97794c85
Chad Nehemiah <chad.nehemiah94@gmail.com>
2025-06-19 11:34:04 -0500

    junkanoo 1.2.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/junkanoo.rb
+++ Formula/junkanoo.rb
@@ -1,8 +1,8 @@
 class Junkanoo < Formula
   desc "Decentralized ephemeral file sharing CLI browser"
   homepage "https://github.com/maschad/junkanoo"
-  url "https://github.com/maschad/junkanoo/releases/download/v1.2.2/junkanoo"
-  sha256 "3198bb7dbb482ed87a439d030a68db01e88eeffdbffb9a829739b0bfcd1b665b"
+  url "https://github.com/maschad/junkanoo/releases/download/v1.2.3/junkanoo-v1.2.3-x86_64-apple-darwin.tar.gz"
+  sha256 "d35d64ae0b52f3a5bfe1b884b0a0a8540b04040af662cb2c8da4beb7760cc570"
   head "https://github.com/maschad/junkanoo.git", branch: "main"
 
   def install
